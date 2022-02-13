5002cbe9dca2eea0e310ef83723f8e6cea422a1a
Bondarenko <Homja4iwe@gmail.com>
2022-02-13 16:23:50 +0300

    Добавил тест функции on_event

--- tests.py
+++ tests.py
@@ -1,10 +1,17 @@
 from unittest import TestCase
-from unittest.mock import patch, Mock
+from unittest.mock import patch, Mock, ANY
+from vk_api.bot_longpoll import VkBotMessageEvent
 
 from bot import Bot
 
 
 class Test1(TestCase):
+    RAW_EVENT = {'type': 'message_new',
+                 'object': {'date': 1644755492, 'from_id': 704002866, 'id': 23, 'out': 0, 'attachments': [],
+                            'conversation_message_id': 22, 'fwd_messages': [], 'important': False,
+                            'is_hidden': False, 'peer_id': 704002866, 'random_id': 0, 'text': 'Привет'},
+                 'group_id': 210610901, 'event_id': '09fbdf95950bb0e2642a78e52320500d5df5ff62'}
+
     def test_run(self):
         count = 5
         obj = {'a': 1}
@@ -21,3 +28,21 @@ class Test1(TestCase):
                 bot.on_event.assert_called()
                 bot.on_event.assert_any_call(obj)
                 assert bot.on_event.call_count == count
+
+    def test_on_event(self):
+        event = VkBotMessageEvent(raw=self.RAW_EVENT)
+
+        send_mock = Mock()
+        with patch('bot.VkApi'):
+            with patch('bot.VkBotLongPoll'):
+                bot = Bot('', '')
+                bot.api = Mock()
+                bot.api.messages.send = send_mock
+
+                bot.on_event(event)
+
+        send_mock.assert_called_once_with(
+            message=self.RAW_EVENT['object']['text'],
+            random_id=ANY,
+            peer_id=self.RAW_EVENT['object']['peer_id'],
+        )
